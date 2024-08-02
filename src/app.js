a347f112b99fa7479bdde62fa77d409d44277b70
Ritik-Modi <ritikmodi755@gmail.com>
2024-08-02 17:31:48 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(
@@ -14,5 +15,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declarations
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
 
 export { app };
